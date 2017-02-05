faa7331ac2b0bcf388c005b112c413b82ee2ed17
Karim Kilani Jrad <karim.kilanijrad@esprit.tn>
2017-02-05 20:11:26 +0100

    projetController

--- src/Esprit/socialproBundle/Controller/ProjetsController.php
+++ src/Esprit/socialproBundle/Controller/ProjetsController.php
@@ -10,4 +10,18 @@ class ProjetsController extends Controller
     {
         return $this->render('EspritsocialproBundle:Projets:indexprojets.html.twig');
     }
-}
+
+    public function tachesAction()
+    {
+        return $this->render('EspritsocialproBundle:Projets:gestiontaches.html.twig');
+    }
+
+    public function equipesAction()
+    {
+        return $this->render('EspritsocialproBundle:Projets:gestionequipes.html.twig');
+    }
+
+    public function gestionprojetsAction()
+    {
+        return $this->render('EspritsocialproBundle:Projets:gestionprojets.html.twig');
+    }}
