0c75ac0c43f06b7701aa48a137ace1f8f69c9130
Midani Rachdi <midani.rachdi@esprit.tn>
2017-02-06 20:04:36 +0100

    *Quand un utilisateur est authentifié et tente d'accéder à /login ,il sera redirigé vers l'acceuil *J'ai supprimé le dossier Controller sous app/Resources/FOSUserBundle

---
+++ src/Esprit/socialproBundle/Controller/SecurityController.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Esprit\socialproBundle\Controller;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\UserBundle\Controller\SecurityController as BaseController;
+
+class SecurityController extends BaseController
+{
+
+    public function loginAction(Request $request)
+    {
+        $authChecker = $this->get('security.authorization_checker');
+
+        if ($authChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('espritsocialpro_homepage');
+        }
+
+        return parent::loginAction($request);
+    }
+
+
+}
