2e6f18fc4e88cb70e21fced1678dcfd0ab6e0a08
Nadia Zayene <nadia.zayene@esprit.tn>
2017-02-12 17:59:29 +0100

    modification des departements

--- src/Esprit/socialproBundle/Controller/DepartementController.php
+++ src/Esprit/socialproBundle/Controller/DepartementController.php
@@ -12,7 +12,8 @@ class DepartementController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('EspritsocialproBundle:Departement:indexdepartement.html.twig');
+        $departements=$this->getDoctrine()->getManager()->getRepository("EspritsocialproBundle:Departement")->findAll();
+        return $this->render("@Espritsocialpro/Departement/indexdepartement.html.twig",array('departements'=>$departements));
     }
 
     public function affectationAction()
@@ -21,8 +22,19 @@ class DepartementController extends Controller
 
     }
 
-    public function ajoutAction(Request $Request)
+    public function ajoutAction(Request $request)
     {
+        if ($request->isMethod("post")){
+            $departement=new Departement();
+            $departement->setNomDepartement($request->get("nom"));
+            $id=$request->get("chef");
+            $chef=$this->getDoctrine()->getManager()->getRepository("EspritsocialproBundle:User")->find($id);
+            $departement->setChef($chef);
+            $this->getDoctrine()->getManager()->persist($departement);
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute("espritsocialpro_departements");
+
+        }
         $users=$this->getDoctrine()->getManager()->getRepository("EspritsocialproBundle:User")->findAll();
         $usersCh=array();
         foreach($users as $user){
@@ -31,10 +43,35 @@ class DepartementController extends Controller
         }
         return $this->render("@Espritsocialpro/Departement/ajoutdepartement.html.twig",array("users"=>$usersCh));
     }
-    public function afficheAction(Request $Request){
 
-        $departements=$this->getDoctrine()->getManager()->getRepository("EspritsocialproBundle:Departement")->findAll();
-        return $this->render("@Espritsocialpro/Departement/affiche.html.twig",array('departements'=>$departements));
+    public  function supprimerAction($id){
+        $dep=$this->getDoctrine()->getManager()->getRepository("EspritsocialproBundle:Departement")->find($id);
+        $this->getDoctrine()->getManager()->remove($dep);
+        $this->getDoctrine()->getManager()->flush();
+        return $this->redirectToRoute('espritsocialpro_departements');
+
+    }
+    public function modifierAction(Request $request,$id){
+        $em = $this->getDoctrine()->getManager();
+        $dep=$em->getRepository("EspritsocialproBundle:Departement")->find($id);
+        $users=$this->getDoctrine()->getManager()->getRepository("EspritsocialproBundle:User")->findAll();
+        $usersCh=array();
+        foreach($users as $user){
+            if ($user->hasRole("ROLE_CHEF_DEP"))
+                array_push($usersCh,$user);
+        }
+
+        if($request->isMethod("post"))
+        {
+            $dep->setId($request->get("id"));
+            $dep->setNom($request->get("nomDepartement"));
+            $dep->setNom($request->get("chef"));
+            $em->persist($dep);
+            $em->flush();
+            return $this->redirectToRoute("espritsocialpro_departements");
+        }
+        return $this->render("@Espritsocialpro/Departement/modifierpartement.html.twig",array("dep"=>$dep,"users"=>$usersCh));
+
     }
 
 
