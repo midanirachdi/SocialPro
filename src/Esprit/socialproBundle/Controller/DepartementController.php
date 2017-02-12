d16141236b96777d936480bd746e6a93ee49b088
Nadia Zayene <nadia.zayene@esprit.tn>
2017-02-12 15:26:42 +0100

    interface ajout departement routing.yml departement controlleur

--- src/Esprit/socialproBundle/Controller/DepartementController.php
+++ src/Esprit/socialproBundle/Controller/DepartementController.php
@@ -2,7 +2,11 @@
 
 namespace Esprit\socialproBundle\Controller;
 
+use Esprit\socialproBundle\Entity\Departement;
+use Esprit\socialproBundle\Form\DepartementType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DepartementController extends Controller
 {
@@ -15,4 +19,23 @@ class DepartementController extends Controller
     {
         return $this->render('EspritsocialproBundle:Departement:gestionaffectations.html.twig');
 
-    }}
+    }
+
+    public function ajoutAction(Request $Request)
+    {
+        $users=$this->getDoctrine()->getManager()->getRepository("EspritsocialproBundle:User")->findAll();
+        $usersCh=array();
+        foreach($users as $user){
+            if ($user->hasRole("ROLE_CHEF_DEP"))
+                array_push($usersCh,$user);
+        }
+        return $this->render("@Espritsocialpro/Departement/ajoutdepartement.html.twig",array("users"=>$usersCh));
+    }
+    public function afficheAction(Request $Request){
+
+        $departements=$this->getDoctrine()->getManager()->getRepository("EspritsocialproBundle:Departement")->findAll();
+        return $this->render("@Espritsocialpro/Departement/affiche.html.twig",array('departements'=>$departements));
+    }
+
+
+}
