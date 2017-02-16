f8005b8c78c2ffbdbf89a9a024a5195d0e2ab93e
Yassin Abderrabba <mohamedyassin.abderrabba@esprit.tn>
2017-02-16 19:29:05 +0100

    ******

--- src/Esprit/socialproBundle/Controller/CertificationController.php
+++ src/Esprit/socialproBundle/Controller/CertificationController.php
@@ -5,6 +5,7 @@ namespace Esprit\socialproBundle\Controller;
 use Esprit\socialproBundle\Entity\Certification;
 use Esprit\socialproBundle\Entity\Competence;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 
 
@@ -23,34 +24,38 @@ class CertificationController extends Controller
     public function AjouterCertificationAction(Request $req){
         $em = $this->getDoctrine()->getManager();
         $user=$this->getUser();
-
-        $c=new Certification();
-        $c->setUtilisateur($user);
-        $c->setDescriptionCertification($req->get("description"));
-        $expiration = $req->get("duree");
-        $date = new \DateTime($req->get("datenaissance"));
-        $c->setDateexpiration($date);
+        $date = new \DateTime();
+        $datefinale=$req->get("duree");
+        $dateexpiration=$date->add(new \DateInterval('P'.$datefinale.'Y'));
+        $certif=new Certification();
+        $certif->setUtilisateur($user);
+        $certif->setDescriptionCertification($req->get("description"));
+        $certif->setDateexpiration($dateexpiration);
 
         if($req->get("nomcomp")=="")
         {
             $competence = $em->getRepository('EspritsocialproBundle:Competence')->find($req->get("compconnue"));
-
-            $c->setCompetence($competence);
-
+            $certif->setCompetence($competence);
         }
         else
             {
-            $comp=new Competence();
-            $comp->setDescription($req->get("nomcomp"));
-                $em->persist($comp);
-                $em->flush();
-                $competence = $em->getRepository('EspritsocialproBundle:Competence')->find($req->get("compconnue"));
-                $c->setCompetence(3);
-
-            }
-        $em->persist($c);
+                $comp=new Competence();
+                $a = $em->getRepository('EspritsocialproBundle:Competence')->findOneBy(array('description' => ($req->get("nomcomp"))));
+                if (is_null($a)==true)
+                {
+                    $comp->setDescription($req->get("nomcomp"));
+                    $em->persist($comp);
+                    $em->flush();
+                    $competence = $em->getRepository('EspritsocialproBundle:Competence')->findOneBy(array('description' => ($req->get("nomcomp"))));
+                    $certif->setCompetence($competence);
+                }
+                else{
+                    $competence = $em->getRepository('EspritsocialproBundle:Competence')->findOneBy(array('description' => ($req->get("nomcomp"))));
+                $certif->setCompetence($competence);
+            }}
+        $em->persist($certif);
         $em->flush();
-        return $this->render('@Espritsocialpro/Profil/profil.html.twig');
+        return $this->redirectToRoute('espritsocialpro_profil');
 
     }
 
@@ -67,7 +72,18 @@ class CertificationController extends Controller
     public function ajouterimageAction($id)
     {
 
+
+
     }
+
+    public function effacercertificationAction($id)
+    {   $em = $this->getDoctrine()->getManager();
+        $certification = $em->getRepository('EspritsocialproBundle:Certification')->find($id);
+        $em->remove($certification);
+        $em->flush();
+        return $this->redirectToRoute('espritsocialpro_profil');
+    }
+
     public function AfficherCompetenceAction(){
         $em = $this->getDoctrine()->getManager();
         $competences = $em->getRepository('EspritsocialproBundle:Competence')->findcompetences();
